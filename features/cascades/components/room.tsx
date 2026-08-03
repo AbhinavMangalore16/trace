f9f1672d6c12971f4df08ca8c66698916e58997b
abhinavMangalore16 <abhinavm16104@gmail.com>
2026-08-04 03:02:29 +0530

    room websocket canvas for collab-II

--- features/cascades/components/room.tsx
+++ features/cascades/components/room.tsx
@@ -9,7 +9,7 @@ import {
 
 export function Room({ roomId, children }: { roomId: string, children: ReactNode }) {
     return (
-        <LiveblocksProvider throttle={20} publicApiKey={"pk_dev_VwRM1RUE_YJt5mk7WJRsOutQZ_jVIE3Y5hB3zb2mUGRQUdYTdCY2UDb1RF1m0qX2"}>
+        <LiveblocksProvider throttle={20} publicApiKey={process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY!}>
             <RoomProvider id={roomId}>
                 <ClientSideSuspense fallback={<div>Loading…</div>}>
                     {children}
